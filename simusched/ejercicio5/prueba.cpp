64462e83ed2a29b931a54d1c2638a40630eb7b35
Lucía Rabinowicz <lu.rabinowicz@gmail.com>
2016-04-29 17:19:35 -0300

    vamos viendo si lee...

--- simusched/ejercicio5/prueba.cpp
+++ simusched/ejercicio5/prueba.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 #include <sstream>
+#include <vector>
 
 using namespace std;
-
+/*
 int main()
 {
 	string st;
 	getline(cin,st);
 	istringstream iss;
-	iss.str(st);
-	iss.clear();
+	iss.str(st); //hay que ponerlo
+	iss.clear(); //hay que ponerlo
 	string s;
 	iss >> s;
 	cout << s << " ";
@@ -22,4 +23,39 @@ int main()
 			cout << "LUyManu" << endl;
 		}
 	}
+}
+*/
+
+int main() {
+	vector<int> tiempos;
+	for(int i = 0; i<6; i++){
+		tiempos[i] = 0;
+	}
+	string st;
+	string s;
+	getline(cin,st);
+	istringstream iss;
+	iss.str(st); //hay que ponerlo
+	iss.clear(); //hay que ponerlo
+	iss >> s;
+	if(s == "CPU") {
+		int a,b,c;
+		iss >> a >> b >> c;
+		tiempos[b]++;
+	}
+
+	while(s != ""){
+
+		getline(cin,st);
+		istringstream iss;
+		iss.str(st); //hay que ponerlo
+		iss.clear(); //hay que ponerlo
+		iss >> s;
+		if(s == "CPU") {
+			int a,b,c;
+			iss >> a >> b >> c;
+			tiempos[b]++;
+		}
+	}
+
 }
